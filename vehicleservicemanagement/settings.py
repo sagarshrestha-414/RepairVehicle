000328136144f06b6849c3971a6e4048a316fcdc
sagarshrestha-414 <saagardhoju414@gmail.com>
2022-03-22 18:42:10 +0545

    hello

--- vehicleservicemanagement/settings.py
+++ vehicleservicemanagement/settings.py
@@ -23,7 +23,7 @@ ALLOWED_HOSTS = ['localhost','repairgarage.herokuapp.com']
 
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_noststic'
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
